73bd3c8b818d533eafa8bf4a2825eec61d742b44
Ben Haus <ben@little-haus.com>
2020-03-24 17:34:02 -0400

    MVP

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { v4 as uuid } from "uuid";
+import Form from "./Components/Form";
+
+import "./App.css";
+
+const initialTeam = [
+  { id: uuid(), name: "Ben Haus", email: "ben@little-haus.com", role: "Web Developer" }
+];
 
 function App() {
+  const [teamMembers, setTeamMembers] = useState(initialTeam);
+  const [formValues, setFormValues] = useState({
+    name: "",
+    role: "",
+    email: ""
+  });
+
+  const onInputChange = event => {
+    const inputChanged = event.target.name;
+    const newInputValue = event.target.value;
+
+    setFormValues({
+      ...formValues,
+      [inputChanged]: newInputValue
+    });
+  };
+  const onFormSubmit = event => {
+    // stop the form from reloading the page on submit
+    event.preventDefault();
+    // let's add a new friend to the friedns array in state
+    // let's make a new friend
+    const newMember = {
+      id: uuid(),
+      name: formValues.name,
+      email: formValues.email,
+      role: formValues.lname
+    };
+    setTeamMembers([...teamMembers, newMember]); // WE NEED TO PASS AN ENTIRE ARRAY
+    // setFriends(friends.concat(newFriend))
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form
+        onInputChange={onInputChange}
+        formValues={formValues}
+        onFormSubmit={onFormSubmit}
+      />
+
+      <h1>Team Members: </h1>
+      {teamMembers.map(mem => (
+        <div key={mem.id}>
+          {mem.name} {mem.email} {mem.role}
+        </div>
+      ))}
     </div>
   );
 }
---
+++ team-builder/src/Components/Form.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+
+export default function Form (props) {
+
+    return (
+      <form onSubmit={props.onFormSubmit}>
+        <label for="name">Name: </label>
+        <input
+          name="name"
+          type="text"
+          value={props.formValues.name}
+          onChange={props.onInputChange}
+        />
+        <br />
+        <label for="email">Email: </label>
+        <input
+          name="email"
+          type="text"
+          value={props.formValues.email}
+          onChange={props.onInputChange}
+        />
+        <br />
+        <label for="role">Role: </label>
+        <input
+          name="role"
+          type="text"
+          value={props.formValues.role}
+          onChange={props.onInputChange}
+        />
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    );
+}
